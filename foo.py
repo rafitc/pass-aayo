0ab3aec0a80ff5f51c1cd1e4255dd9621b427592
rafitc <rafirasheed.tc99@gmail.com>
2020-05-21 01:17:34 +0530

    bored

--- foo.py
+++ foo.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+import re
+#from datetime import datetime
 
 URL = 'http://results.cusat.ac.in/'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 tables = soup.find_all('ul')
-li = tables[0].find_all('Date of publication')
-print(li)
+li = tables[0].get_text() #date string start from 131:145
+stringStart = li.find(':')
+dateOfPub = str(li[(stringStart):(stringStart+15)])
+
+print(dateOfPub)
+#dateOfPub = dateOfPub.split()
