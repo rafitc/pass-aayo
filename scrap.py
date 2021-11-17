6fe6c74c81bd557749172930f933b7a06366ff75
Rafi <rafirasheed.tc99@gmail.com>
2021-11-17 12:57:08 +0530

    flask error fix

--- scrap.py
+++ scrap.py
@@ -80,7 +80,4 @@ def start():
 
 # main driver function
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application
-    # on the local development server.
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
